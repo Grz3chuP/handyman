48f2622a94e2695320d1d930cfcb952f6cac8283
grzejatol <grzejatol@gmail.com>
2023-10-19 19:49:32 +0100

    initial push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {IndexComponent} from "./index/index.component";
+import {GalleryComponent} from "./gallery/gallery.component";
+import {ContactComponent} from "./contact/contact.component";
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'index', component: IndexComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'contact', component: ContactComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    GalleryComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
