321bf0166b227b11d64a071d1dfb9483731fc6e5
grzejatol <grzejatol@gmail.com>
2023-10-22 19:29:47 +0100

    composer dalej

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'handyman/handyman',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '98cfd64d69336288fdb8c5ba612e51d4b0d7b3bb',
+        'reference' => 'c2ca3847cfba86d24b5690779c1b58ce6e2706df',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'handyman/handyman' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '98cfd64d69336288fdb8c5ba612e51d4b0d7b3bb',
+            'reference' => 'c2ca3847cfba86d24b5690779c1b58ce6e2706df',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
