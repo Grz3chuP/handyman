35b9df64103024fff8424fd3ef6878c22214a253
grzejatol <grzejatol@gmail.com>
2023-10-23 19:30:35 +0100

    kosmetyk email

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-contact',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class ContactComponent {
 
+
+
+email: string = '';
+message: string = '';
+
+  sendEmail() {
+
+
+  }
 }
