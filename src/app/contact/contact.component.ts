6e7fcb164a47177a7fa9f6a38883857f735486dc
grzejatol <grzejatol@gmail.com>
2023-10-24 18:07:40 +0100

    Email

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
@@ -13,8 +14,32 @@ export class ContactComponent {
 email: string = '';
 message: string = '';
 
-  sendEmail() {
+isValid() {
+  const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+  return emailRegex.test(this.email);
+}
 
-
+  sendEmail(e: Event) {
+    if (this.email == '' || this.message == '') {
+      alert('Please fill in all fields');
+      return;
+    } else if (!this.isValid()) {
+      alert('Please enter a valid email address');
+      return;
+    } else if (this.isValid()) {
+      e.preventDefault();
+      emailjs.sendForm('service_5qtg5lh', 'template_qspkxvo', e.target as HTMLFormElement, '8PQvjepagDVzW_K45')
+        .then((result: EmailJSResponseStatus) => {
+          console.log(result.text);
+          alert('Message sent!');
+        }, (error) => {
+          console.log(error.text);
+          alert('An error occurred' + error.text + ', Please try again');
+        });
+    }
   }
+
+
+
+
 }
